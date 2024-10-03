7c07f49bf26fa59968d35ba850f23fe2814a14ce
MikkelTMS <mtamsgaard@gmail.com>
2024-10-03 13:27:52 +0200

    Mikkel's sjove undskyldninger!
    Ja tak!

--- assets/js/modules/Mikkel/Mikkel.js
+++ assets/js/modules/Mikkel/Mikkel.js
@@ -1,3 +1,33 @@
-export default function setupMikkel() {
-  console.log("Hello from Mikkel");
+export default async function setupMikkel() {
+  return fetch("https://excuser-three.vercel.app/v1/excuse/college")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Mikkel's network response was not ok");
+      }
+      return response.json();
+    })
+
+    .then((data) => {
+      handleJokes(data);
+      return data;
+    })
+
+    .catch((error) => {
+      console.log(error);
+      return error;
+    });
+}
+
+function handleJokes() {
+  const jokeDisplay = document.getElementById("Mikkel-p");
+  const jokeButton = document.getElementById("Mikkel-button");
+
+  jokeButton.addEventListener("click", () => {
+    fetch("https://excuser-three.vercel.app/v1/excuse/college") // replace with your API endpoint
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data[0].excuse);
+        jokeDisplay.innerHTML = `<p>${data[0].excuse}</p>`; // assuming the joke is in the "joke" property
+      });
+  });
 }
